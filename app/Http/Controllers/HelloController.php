ed663d569df0d002dfec122fc3d5f9f1762745af
thomas-in-jpn <hi-toshi@outlook.com>
2024-03-08 22:28:22 +0900

    ビューコンポーザで作成

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,12 +9,7 @@ class HelloController extends Controller
 {
     // methodを追加
     public function index(){
-        $data = [
-            ['name'=>'山田たろう', 'mail'=>'taro@yamada.local'],
-            ['name'=>'田中はなこ', 'mail'=>'hanako@flower.local'],
-            ['name'=>'鈴木さちこ', 'mail'=>'sachiko@happy.local']
-        ];
-        return view('hello.index',['data'=>$data]);
+        return view('hello.index',['message'=>'hello!']);
     }
     
 }
