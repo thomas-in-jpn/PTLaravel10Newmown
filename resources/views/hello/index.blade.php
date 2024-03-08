c5fd6bed515fcec212eb59dc53d6ee36aa46e83b
thomas-in-jpn <hi-toshi@outlook.com>
2024-03-08 20:56:55 +0900

    @ifディレクティブを使って、表示する文字列を変更する

---
+++ resources/views/hello/index.blade.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>Hello/Index</title>
+        <style>
+            body {font-size: 16pt; color:#999; }
+            h1 { font-size:100pt; text-align:right; color:#eee;
+            margin: -40px 0px -50px 0px; }
+        </style>
+    </head>
+    <body>
+         <h1>Blade/Index</h1>
+         @if ($msg != '')
+         <p>こんにちは、{{$msg}}さん。</p>
+         @else
+         <p>なにか書いて下さい。</p>
+         @endif
+         <form method="post" action="/laravelapp/hello">
+            @csrf
+            <input type="text" name="msg">
+            <input type="submit">
+        </form>
+    </body>            
+</html>
